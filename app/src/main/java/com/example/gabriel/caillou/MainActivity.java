7b06b58f78ffd7b5c9d1f2a5b5c917033ab42e09
Gabe <gabelara@knights.ucf.edu>
2018-03-21 14:41:51 -0400

    initial commit

---
+++ app/src/main/java/com/example/gabriel/caillou/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.gabriel.caillou;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView signUp;
+    EditText usernameTextBox;
+    EditText passwordTextBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        signUp = (TextView) findViewById(R.id.textView);
+
+        signUp.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view)
+            {
+                System.out.println("SignUp Activity Started");
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void login(View view) throws JSONException {
+        String username, password;
+        JSONObject payload = new JSONObject(); //JSON Exception thrown must be applied after.
+
+        usernameTextBox = (EditText) findViewById(R.id.editText);
+        passwordTextBox = (EditText) findViewById(R.id.editText2);
+
+        username = usernameTextBox.getText().toString();
+        password = passwordTextBox.getText().toString();
+
+        payload.put("username", username);
+        payload.put("password", password);
+
+        if (payload != null) // This function should check the validity of the object and check if
+                             // payload was sent and secured.
+        {
+            // Set up a dummy toast message
+            Toast.makeText(getApplicationContext(), "Hello " + username, Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/gabriel/caillou/SignUpActivity.java
@@ -0,0 +1,45 @@
+package com.example.gabriel.caillou;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    Button signUpButton;
+    EditText firstNameTextBox, lastNameTextBox, passwordTextBox, emailTextBox;
+    String firstName, lastName, password, email;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        signUpButton = findViewById(R.id.signUpButton);
+        firstNameTextBox = findViewById(R.id.firstNameTextBox);
+        lastNameTextBox = findViewById(R.id.lastNameTextBox);
+        passwordTextBox = findViewById(R.id.passwordTextBox);
+        emailTextBox = findViewById(R.id.emailTextBox);
+
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firstName =  firstNameTextBox.getText().toString();
+                lastName = lastNameTextBox.getText().toString();
+                password = passwordTextBox.getText().toString();
+                email = emailTextBox.getText().toString();
+
+                System.out.println(firstName + " " + lastName + " " + password + " " + email);
+                //Toast.makeText(getApplicationContext(), (firstName + " " + lastName + " " + password + " " + email) , Toast.LENGTH_SHORT).show();
+
+
+                finish();
+            }
+        });
+
+    }
+}
