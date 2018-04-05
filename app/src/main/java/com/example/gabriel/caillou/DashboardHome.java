b31b3987a51fec37445a1d525cf1ab84906374a3
Gabe <gabelara@knights.ucf.edu>
2018-04-04 22:20:20 -0400

    Navigation Drawer touch slide works. TODO: fixed drawer open fragments, user profile view/image on drawer, design schemes (if applicable).

--- app/src/main/java/com/example/gabriel/caillou/DashboardHome.java
+++ app/src/main/java/com/example/gabriel/caillou/DashboardHome.java
@@ -3,26 +3,49 @@ package com.example.gabriel.caillou;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 public class DashboardHome extends AppCompatActivity
         implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-
+    // Navigation view drawer
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard_home);
 
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(this);
 
+        // Straight-Line Code format for NavigationItemSelectedListener
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        // set item as selected to persist highlight
+                        item.setChecked(true);
+
+                        // Close drawer when item is tapped.
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                        //item.getItemId()
+                    }
+                }
+        );
+
         // When application is new loaded (tabs haven't been click) then load the first fragment
         loadFragment(new HomeFragment());
+
+
     }
 
     // Method that loads the fragment
@@ -36,7 +59,6 @@ public class DashboardHome extends AppCompatActivity
         }
 
         return false;
-        // Jason Test 2
     }
 
     @Override
