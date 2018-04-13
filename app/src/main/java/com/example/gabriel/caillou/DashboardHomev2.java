a16d455ed40cc995105406a010901cdab6ef7fc3
Gabriel <gabelara@knights.ucf.edu>
2018-04-12 23:39:07 -0400

    Change navigation drawer options/icons. Logout function closes activity. TODO: remove user profile (state) contents for the next user to login.

--- app/src/main/java/com/example/gabriel/caillou/DashboardHomev2.java
+++ app/src/main/java/com/example/gabriel/caillou/DashboardHomev2.java
@@ -159,25 +159,15 @@ public class DashboardHomev2 extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera)
+        if (id == R.id.nav_profile)
         {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery)
+        } else if (id == R.id.nav_teams)
         {
 
-        } else if (id == R.id.nav_slideshow)
+        } else if (id == R.id.nav_logout)
         {
-
-
-        } else if (id == R.id.nav_manage)
-        {
-
-        } else if (id == R.id.nav_share)
-        {
-
-        } else if (id == R.id.nav_send)
-        {
-
+            finish();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
